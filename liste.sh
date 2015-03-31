df6dd629bdc422c021989611b454ea456882f2eb
Findus23 <l.winkler23@me.com>
2015-03-31 11:55:01 +0200

    Bessere Webseiten-Filterung

--- liste.sh
+++ liste.sh
@@ -4,11 +4,11 @@ rm descr.txt
 
 for i in {0..25}
 do
-	wget -O download.tmp http://www.ikea.com/gb/en/catalog/productsaz/$i/
+	wget -O download.tmp http://www.ikea.com/at/de/catalog/productsaz/$i/
 	grep "productsAzLink" download.tmp > lines.tmp
 	while read line; do
-		name=$(echo $line | xmllint --xpath 'string(//a)' -)
-		ikeaname=$(echo "$name" | egrep -o "((([A-Z])+){2,} *)+")
+		name=$(echo $line | grep -P -o "(?<=/\">).*(?=</a>)")
+		ikeaname=$(echo "$name" | egrep -o "(((([A-Z])+){2,}|\&) *)+")
 		descr=${name#$ikeaname}
 		echo $ikeaname >> ikeaname.txt
 		echo $descr >> descr.txt
