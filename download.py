e9e75d6bf0b9cbb133d7eb5e2576e06d4f5508d4
Lukas Winkler <l.winkler23@mailbox.org>
2016-07-19 14:53:49 +0200

    Neu in Python

---
+++ download.py
@@ -0,0 +1,23 @@
+import json
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+descriptions = []
+names = []
+for i in range(0, 25):
+    r = requests.get("http://www.ikea.com/at/de/catalog/productsaz/{letter}/".format(letter=i))
+    soup = BeautifulSoup(r.text, 'html.parser')
+    for span in soup.find_all('span', "productsAzLink"):
+        product = span.a.string
+        m = re.match("((?:[^a-z\s]|Ä|Å|Ö){2,})? ?(.*)?", product)
+        print(product)
+        names.append(m.group(1))
+        descriptions.append(m.group(2))
+data = {
+    "descriptions": list(set(descriptions)),
+    "names": list(set(names))
+}
+with open('download.json', 'w') as outfile:
+    json.dump(data, outfile, indent=4)
